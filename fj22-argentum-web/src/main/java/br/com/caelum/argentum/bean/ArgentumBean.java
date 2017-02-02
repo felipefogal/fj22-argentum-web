fe61c202ca7ceb62e325a42c57233b974f0296e8
lab6601 <lab6601@caelum144-06>
2017-02-02 20:35:56 -0200

    Exercicios 7.10 e 7.12 - Formatando dados com JSF

---
+++ fj22-argentum-web/src/main/java/br/com/caelum/argentum/bean/ArgentumBean.java
@@ -0,0 +1,26 @@
+package br.com.caelum.argentum.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.caelum.argentum.modelo.Negociacao;
+import br.com.caelum.argentum.ws.ClienteWebService;
+
+@ViewScoped
+@ManagedBean
+public class ArgentumBean implements Serializable {
+
+	private List<Negociacao> negociacoes;
+	
+	public ArgentumBean(){
+		negociacoes = new ClienteWebService().getNegociacoes();
+	}
+	
+	public List<Negociacao> getNegociacoes(){
+		return negociacoes;
+	}
+	
+}
