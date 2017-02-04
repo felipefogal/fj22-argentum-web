b13dc3a6390887e1d52be20a5c6bbeac42e6e263
lab6601 <lab6601@caelum144-06>
2017-02-03 23:11:27 -0200

    teste

--- fj22-argentum-web/src/main/java/br/com/caelum/argentum/bean/ArgentumBean.java
+++ fj22-argentum-web/src/main/java/br/com/caelum/argentum/bean/ArgentumBean.java
@@ -1,6 +1,7 @@
 package br.com.caelum.argentum.bean;
 
 import java.io.Serializable;
+import java.lang.reflect.Constructor;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -9,6 +10,7 @@ import javax.faces.bean.ViewScoped;
 import org.primefaces.model.chart.ChartModel;
 
 import br.com.caelum.argentum.grafico.GeradorModelGrafico;
+import br.com.caelum.argentum.indicadores.Indicador;
 import br.com.caelum.argentum.indicadores.IndicadorFechamento;
 import br.com.caelum.argentum.indicadores.MediaMovelSimples;
 import br.com.caelum.argentum.modelo.Candlestick;
@@ -23,23 +25,62 @@ public class ArgentumBean implements Serializable {
 
 	private List<Negociacao> negociacoes;
 	private ChartModel modeloGrafico;
+	private String nomeMedia;
+	private String nomeIndicadorBase;
 	
+	public String getNomeMedia() {
+		return nomeMedia;
+	}
+
+	public void setNomeMedia(String nomeMedia) {
+		this.nomeMedia = nomeMedia;
+	}
+
 	public ChartModel getModeloGrafico() {
 		return modeloGrafico;
 	}
 	
+	public String getNomeIndicadorBase() {
+		return nomeIndicadorBase;
+	}
+
+	public void setNomeIndicadorBase(String nomeIndicadorBase) {
+		this.nomeIndicadorBase = nomeIndicadorBase;
+	}
+	
 	public ArgentumBean(){
 		this.negociacoes = new ClienteWebService().getNegociacoes();
+		geraGrafico();
+	}
+
+	public void geraGrafico() {
+		System.out.println("PLOTANDO: " + nomeMedia + " de " + nomeIndicadorBase);
 		List<Candlestick> candles = new CandlestickFactory().constroiCandles(negociacoes);
 		SerieTemporal serie = new SerieTemporal(candles);
 		GeradorModelGrafico geradorGrafico = 
 				new GeradorModelGrafico(serie, 2, serie.getUltimaPosicao());
-		geradorGrafico.plotaIndicador(new MediaMovelSimples(new IndicadorFechamento()));
+		geradorGrafico.plotaIndicador(defineIndicador());
 		this.modeloGrafico = geradorGrafico.getModeloGrafico();
 	}
 	
+	private Indicador defineIndicador() {
+		if (nomeIndicadorBase == null || nomeMedia == null)
+			return new MediaMovelSimples(new IndicadorFechamento());
+		try {
+			String pacote = "br.com.caelum.argentum.indicadores.";
+			Class<?> classeIndicadorBase = Class.forName(pacote + nomeIndicadorBase);
+			Indicador indicadorBase = (Indicador) classeIndicadorBase.newInstance();
+		
+			Class<?> classeMedia = Class.forName(pacote + nomeMedia);
+			Constructor<?> construtorMedia = classeMedia.getConstructor(Indicador.class);
+			Indicador indicador = (Indicador) construtorMedia.newInstance(indicadorBase);
+			return indicador;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public List<Negociacao> getNegociacoes(){
 		return negociacoes;
-	}
-	
+	}	
 }
